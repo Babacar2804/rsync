a1c3740d51191f637439621be221b11bd22a8fae
Babacar2804 <babacarg836@gmail.com>
2024-04-05 21:25:57 +0200

    sauv

--- rsync.sh
+++ rsync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 crontab -e
- 10 * * * * rsync –avz --delete /etc/haproxy/haproxy.cfg root@10.0.2.15:/root/BackupHA/
- 10 * * * * rsync –avz --delete /etc/apache2/apache2.conf root@10.0.2.15:/root/BackupApa/
+ 10 * * * * rsync –avz  /etc/haproxy/haproxy.cfg root@10.0.2.15:/root/BackupHA/
+ 10 * * * * rsync –avz  /etc/apache2/apache2.conf root@10.0.2.15:/root/BackupApa/
 
